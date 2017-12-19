e2af0b3aa2bf7303b4f5650734191acf3718518d
Ajay_Kumar <ajaykumarsampath@gmail.com>
2017-12-19 20:22:37 +0100

    #2 class for the scenario tree

---
+++ matlab/@scenarioTree/scenarioTree.m
@@ -0,0 +1,89 @@
+classdef scenarioTree
+    
+    properties( GetAccess = public, SetAccess = private)
+        stage;
+        value;
+        prob;
+        leaves;
+        ancestor;
+        children;
+        branchingFactor;
+    end 
+
+    methods( Access = public )
+        function obj = scenarioTree( scenarioTreeParameter )
+            %
+            % The scenarioTree class is used to define the propagation of
+            % uncertanity. This is created by passing the structure - 
+            % branching factor. The uncertainty that account at the each
+            % node of the scenario-tree is created from the data that 
+            % represent the uncertainty. When uncertanity is absent, 
+            % random data from uniformly sampled distribution is used.
+            %
+            % Syntax:
+            %   tree = scenarioTree(scenarioTreeParameter);
+            %
+            % INPUT:
+            %   scenarioTreeParameter : scenrio tree parameters 
+            %     Fields of scenarioTreeParameter
+            %       nx : length of the uncertainty
+            %       N  : prediction horizon of the scenario tree 
+            %       branchingFactor : branchingFactor of the scenario tree
+            %       uncertainData : uncertanity data that will used to
+            %         generate the scenario tree 
+            % 
+            % TODO
+            %   Implement functions to generate the scenario tree from the
+            %     uncertainData
+            
+            if(isfield(scenarioTreeParameter, 'uncertainData'))
+                
+            else
+                nx = scenarioTreeParameter.nx;
+                branchingFactorStage = length(scenarioTreeParameter.branchingFactor);
+                obj.branchingFactor = ones(scenarioTreeParameter.N, 1);
+                obj.branchingFactor(1:branchingFactorStage) = scenarioTreeParameter.branchingFactor;
+                obj.stage = 0;
+                obj.prob = 1;
+                obj.leaves = 1;
+                obj.ancestor = 0;
+                obj.value(1, :) = zeros(1, nx);
+                Ns = 1;
+                for iPred = 1:scenarioTreeParameter.N
+                    Ns = Ns*obj.branchingFactor(iPred);
+                    currentNodeVec = obj.leaves(end) + 1:obj.leaves(end) + Ns;
+                    obj.ancestor(currentNodeVec) = kron(obj.leaves, ones(1, obj.branchingFactor(iPred)));
+                    obj.stage(currentNodeVec) = iPred*ones(Ns, 1);
+                    obj.value(currentNodeVec, :) = 0.1*rand(Ns, nx);
+                    if(iPred <= branchingFactorStage)
+                        pd = rand(1, Ns);
+                        if(iPred == 1)
+                            obj.prob(currentNodeVec) = pd/sum(pd);
+                            obj.children{obj.leaves} = currentNodeVec;
+                        else
+                            for i = 1:obj.branchingFactor(iPred - 1)
+                               currentbranch = obj.branchingFactor(iPred);
+                               pd((i-1)*currentbranch + 1: i*currentbranch) = pd((i-1)*currentbranch + 1:...
+                                   i*currentbranch)/sum(pd((i-1)*currentbranch + 1: i*currentbranch));
+                               obj.children{obj.leaves(i)} = currentNodeVec((i-1)*currentbranch + 1: i*currentbranch);
+                            end
+                            obj.prob(currentNodeVec) = pd;
+                        end
+                    else
+                        obj.prob(currentNodeVec) = pd;
+                        for i = 1:length(obj.leaves)
+                            obj.children{obj.leaves(i)} = currentNodeVec(i);
+                        end
+                    end
+                    obj.leaves = currentNodeVec;
+                end
+            end
+        end 
+
+        [sys,V,tree] = system_generation(obj,ops_sys_masses,ops_tree);
+
+        obj = Precondtion_system(obj);
+
+        [ value, primal_epsilon ] = cost_function( obj,Z);
+    end 
+end
